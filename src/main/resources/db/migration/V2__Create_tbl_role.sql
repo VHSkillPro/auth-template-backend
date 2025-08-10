eade1653206566964330e321e16437a9d8a3a0ef
Hazzu <hailaconsutu558@gmail.com>
2025-08-10 16:42:29 +0700

    Add SQL migration scripts for role and role_permission tables, and insert permissions for roles

---
+++ src/main/resources/db/migration/V2__Create_tbl_role.sql
@@ -0,0 +1,10 @@
+-- Create table role (PostgreSQL)
+
+CREATE TABLE tbl_role (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
