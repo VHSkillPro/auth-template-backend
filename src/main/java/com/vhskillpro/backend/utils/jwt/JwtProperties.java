bdb6dfe24881359cdfdcc54eaeab7c2a316eb3e2
Hazzu <hailaconsutu558@gmail.com>
2025-08-15 15:08:59 +0700

    Add JWT configuration properties and update environment variables in sample files

---
+++ src/main/java/com/vhskillpro/backend/utils/jwt/JwtProperties.java
@@ -0,0 +1,14 @@
+package com.vhskillpro.backend.utils.jwt;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "application.security.jwt")
+public class JwtProperties {
+  private String secretKey;
+  private Long accessTokenExpiration;
+  private Long refreshTokenExpiration;
+}
