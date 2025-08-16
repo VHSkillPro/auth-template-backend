a9d7d78429078ea8adee0a81f6ffaacac8b27614
Hazzu <hailaconsutu558@gmail.com>
2025-08-17 00:18:24 +0700

    feat(swagger): enhance Swagger configuration with JWT security scheme

--- src/main/java/com/vhskillpro/backend/configs/SwaggerConfig.java
+++ src/main/java/com/vhskillpro/backend/configs/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.vhskillpro.backend.configs;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +25,17 @@ public class SwaggerConfig {
                 .license(
                     new License()
                         .name("Apache 2.0")
-                        .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+                        .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "bearerAuth",
+                    new SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(In.HEADER)));
   }
 }
